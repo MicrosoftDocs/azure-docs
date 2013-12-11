71ea08b12dddab3699165587ea8b9ce71dddc767
Molly Bostic <mollybos@microsoft.com>
2013-12-11 10:37:02 -0800

    removing extra ps script file

--- migrate.ps1
+++
@@ -1,8 +0,0 @@
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\getting-started-xamarin-ios.md partner-xamarin-notification-hubs-ios-get-started
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\getting-started-xamarin-android.md partner-xamarin-notification-hubs-android-get-started
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\howto-register-user-with-aspnet-ios.md notification-hubs-ios-aspnet-register-user-push-notifications
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\howto-register-user-with-aspnet-windowsdotnet.md notification-hubs-windows-store-aspnet-register-user-push-notifications
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\howto-register-user-with-mobile-service-ios.md notification-hubs-ios-mobile-services-register-user-push-notifications
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\howto-register-user-with-mobile-service-windowsdotnet.md notification-hubs-windows-store-mobile-services-register-user-push-notifications
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\tutorial-notify-users-aspnet.md notification-hubs-aspnet-notify-users
-.\Migrate-Document.ps1 c:\users\mollybos\azure-content-pr\ITPro\Services\notificationhubs\tutorial-notify-users-cross-platform-aspnet.md notification-hubs-aspnet-cross-platform-notify-users
