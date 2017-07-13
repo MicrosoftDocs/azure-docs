3e0ee424221fbaef1b3d9fc11d638ad34b5e522a
Gary Ericson <garye@microsoft.com>
2017-07-13 11:00:36 -0700

    Moved files

---
+++ articles/machine-learning/move.sh
@@ -0,0 +1,7 @@
+mv machine-learning-data-science-virtual-machine-overview.md data-science-virtual-machine/data-science-virtual-machine-overview.md
+mv machine-learning-data-science-vm-do-ten-things.md data-science-virtual-machine/data-science-vm-do-ten-things.md
+mv machine-learning-data-science-provision-vm.md data-science-virtual-machine/data-science-provision-vm.md
+mv machine-learning-data-science-setup-virtual-machine.md data-science-virtual-machine/data-science-setup-virtual-machine.md
+mv machine-learning-data-science-setup-sql-server-virtual-machine.md data-science-virtual-machine/data-science-setup-sql-server-virtual-machine.md
+mv machine-learning-data-science-linux-dsvm-intro.md data-science-virtual-machine/data-science-linux-dsvm-intro.md
+mv machine-learning-data-science-linux-dsvm-walkthrough.md data-science-virtual-machine/data-science-linux-dsvm-walkthrough.md
