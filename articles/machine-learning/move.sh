8fc320ef8919f177e8dea433660489f00c9547bb
Gary Ericson <garye@microsoft.com>
2017-07-13 11:14:55 -0700

    Move files

--- articles/machine-learning/move.sh
+++ articles/machine-learning/move.sh
@@ -1,7 +1,18 @@
-mv machine-learning-data-science-virtual-machine-overview.md data-science-virtual-machine/data-science-virtual-machine-overview.md
-mv machine-learning-data-science-vm-do-ten-things.md data-science-virtual-machine/data-science-vm-do-ten-things.md
-mv machine-learning-data-science-provision-vm.md data-science-virtual-machine/data-science-provision-vm.md
-mv machine-learning-data-science-setup-virtual-machine.md data-science-virtual-machine/data-science-setup-virtual-machine.md
-mv machine-learning-data-science-setup-sql-server-virtual-machine.md data-science-virtual-machine/data-science-setup-sql-server-virtual-machine.md
-mv machine-learning-data-science-linux-dsvm-intro.md data-science-virtual-machine/data-science-linux-dsvm-intro.md
-mv machine-learning-data-science-linux-dsvm-walkthrough.md data-science-virtual-machine/data-science-linux-dsvm-walkthrough.md
+mv machine-learning-live-chat.md team-data-science-process/live-chat.md
+
+
+mv cortana-intelligence-appsource-publishing-guide.md team-data-science-process/cortana-intelligence-appsource-publishing-guide.md
+mv cortana-intelligence-appsource-evaluation-tool.md team-data-science-process/cortana-intelligence-appsource-evaluation-tool.md
+
+
+mv machine-learning-apps-anomaly-detection-api.md team-data-science-process/apps-anomaly-detection-api.md
+
+
+mv cortana-analytics-playbook-predictive-maintenance.md team-data-science-process/cortana-analytics-playbook-predictive-maintenance.md
+mv cortana-analytics-architecture-predictive-maintenance.md team-data-science-process/cortana-analytics-architecture-predictive-maintenance.md
+mv cortana-analytics-technical-guide-predictive-maintenance.md team-data-science-process/cortana-analytics-technical-guide-predictive-maintenance.md
+
+mv cortana-analytics-playbook-vehicle-telemetry.md team-data-science-process/cortana-analytics-playbook-vehicle-telemetry.md
+mv cortana-analytics-playbook-vehicle-telemetry-deep-dive.md team-data-science-process/cortana-analytics-playbook-vehicle-telemetry-deep-dive.md
+mv cortana-analytics-playbook-vehicle-telemetry-powerbi.md team-data-science-process/cortana-analytics-playbook-vehicle-telemetry-powerbi.md
+
