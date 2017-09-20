ef895ad179bc42604e8133619303c553a397ef34
Gary Ericson <garye@microsoft.com>
2017-09-20 13:50:20 -0700

    Clean-up: Deleting illegal and illegitimate files

--- articles/machine-learning/preview/scripts/quick-start-installation/cleanup_libs.ps1
+++
@@ -1,6 +0,0 @@
-# cleanup_libs.ps1
-# Copyright (c) Microsoft. All rights reserved.
-
-# This need to be run as an administrator...
-# Uninstalling all Azure CLI Commands 
-pip freeze | Select-String -SimpleMatch ("azure-") | % {pip uninstall $_ --yes}
