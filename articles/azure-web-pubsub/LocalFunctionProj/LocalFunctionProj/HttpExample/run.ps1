640f23f86250a3ee20b583b5ac632b6f6f7eeece
cebundy <23391945+cebundy@users.noreply.github.com>
2023-03-27 11:46:01 -0700

    updating how-tos

---
+++ articles/azure-web-pubsub/LocalFunctionProj/LocalFunctionProj/HttpExample/run.ps1
@@ -0,0 +1,25 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Write to the Azure Functions log stream.
+Write-Host "PowerShell HTTP trigger function processed a request."
+
+# Interact with query parameters or the body of the request.
+$name = $Request.Query.Name
+if (-not $name) {
+    $name = $Request.Body.Name
+}
+
+$body = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+
+if ($name) {
+    $body = "Hello, $name. This HTTP triggered function executed successfully."
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $body
+})
---
+++ articles/azure-web-pubsub/LocalFunctionProj/LocalFunctionProj/requirements.psd1
@@ -0,0 +1,8 @@
+# This file enables modules to be automatically managed by the Functions service.
+# See https://aka.ms/functionsmanageddependency for additional information.
+#
+@{
+    # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'. 
+    # To use the Az module in your function app, please uncomment the line below.
+    # 'Az' = '9.*'
+}
